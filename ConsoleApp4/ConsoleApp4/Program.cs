9043c2fcd2a881c516cd5cff669cc914a34f8194
Rohan Raj <rohan.raj@nixorcollege.edu.pk>
2024-04-23 21:23:01 +0200

    Initial commit

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3, 11, 7, 8 };
+
+            Array.Sort(arr);
+            Array.Reverse(arr);
+
+            Console.WriteLine(arr[2]);
+
+       
+        }
+    }
+}
---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "Hello Rohan. How are you";
+            for (int i = 0; i < s.Length; i++) 
+            {
+                //Console.WriteLine(s[i].GetType());
+                List<string> l;
+                if (s[i].ToString() != " " && s[i].ToString() != ".")
+                {
+
+                    l.Append(s[i].ToString());
+                    Console.WriteLine(l);
+                    //Console.WriteLine(s[i].ToString() + ':');
+                }
+                
+            };
+        }
+    }
+}
